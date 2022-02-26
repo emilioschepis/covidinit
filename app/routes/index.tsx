f4398dc6bc3b50936cac7f70e7b8e38cb2fc62b4
Emilio Schepis <emilio.schepis@gmail.com>
2022-02-26 22:13:00 +0100

    feat: add footer

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -41,13 +41,15 @@ export default function Index() {
   const data = useLoaderData<NationalTrend>();
 
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold">Dati COVID-19 in Italia</h1>
-      <p className="text-gray-600 font-bold">
-        Dati aggiornati al <time dateTime={data.date}>{dayjs(data.date).format("DD/MM/YYYY HH:mm")}</time>
-      </p>
+    <div className="pt-4 space-y-4">
+      <header className="px-4">
+        <h1 className="text-3xl font-bold">Dati COVID-19 in Italia</h1>
+        <p className="text-gray-600 font-bold">
+          Dati aggiornati al <time dateTime={data.date}>{dayjs(data.date).format("DD/MM/YYYY HH:mm")}</time>
+        </p>
+      </header>
 
-      <div className="grid grid-cols-2 gap-4 mt-4 md:grid-cols-3">
+      <main className="px-4 grid grid-cols-2 gap-2 md:grid-cols-3 md:mb-20">
         <Card icon="🦠" label="Casi totali" value={data.cases_count} variation={data.cases_variation} />
         <Card
           icon="😷"
@@ -73,7 +75,21 @@ export default function Index() {
           variationCanImprove
         />
         <Card icon="🧫" label="Tamponi" value={data.tests_count} variation={data.tests_variation} />
-      </div>
+      </main>
+      <footer className="relative flex flex-col justify-center items-center bottom-0 left-0 w-full p-4 border-t-[1px] border-gray-300 text-sm text-gray-700 bg-white md:fixed">
+        <div>
+          Made with <span aria-label="love">❤️</span> by Emilio Schepis
+        </div>
+        <div className="space-x-2">
+          <a className="text-blue-600" href="https://github.com/emilioschepis/covidinit">
+            Source
+          </a>
+          <span aria-hidden>•</span>
+          <a className="text-blue-600" href="https://twitter.com/emilioschepis">
+            Twitter
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
